8cd8498d042fee0023acac21046debfcec7ac165
Soham Shirke <sohamshirke.215099@gmail.com>
2025-01-24 15:52:32 +0530

    #3 - Nested Serializers & Relations
    - Created nested serializers
    (OrderItems -> Orders)
    - Rendered orders_list view
    - Used SerializerMethodField

--- Starter Code/api/serializers.py
+++ Starter Code/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product
+from .models import *
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +10,37 @@ class ProductSerializer(serializers.ModelSerializer):
         if value <= 0:
             raise serializers.ValidationError("Price must be greater than 0")
         return value
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name', read_only = True)
+    product_price = serializers.DecimalField(source='product.price', read_only = True, max_digits=10, decimal_places=2)
+    class Meta:
+        model = OrderItem
+        fields = (
+            'product_name',
+            'product_price',
+            'quantity',
+            'item_subtotal',
+        )
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only = True) #not required when we create this serializer
+    total_price = serializers.SerializerMethodField()
+    
+    def get_total_price(self, obj):
+        total = 0
+        for item in obj.items.all():
+            total += item.item_subtotal
+        return total
+    
+    class Meta:
+        model = Order
+        fields = (
+            'order_id',
+            'user',
+            'created_at',
+            'status',
+            'items',
+            'total_price',
+        )
+        
--- Starter Code/api/views.py
+++ Starter Code/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
-from api.serializers import ProductSerializer
-from api.models import Product
+from api.serializers import *
+from api.models import *
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -9,10 +9,16 @@ from rest_framework.decorators import api_view
 def product_list(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+    return Response(serializer.data) #Response is a DRF class that converts data to JSON or other formats
 
-@api_view(['GET'])  #converts django view to DRF view
+@api_view(['GET'])  
 def product_detail(request, pk):
     products = get_object_or_404(Product, pk = pk)
     serializer = ProductSerializer(products)
     return Response(serializer.data)
+
+@api_view(['GET'])  
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True) #since it an array of objects we use many=True
+    return Response(serializer.data) 
